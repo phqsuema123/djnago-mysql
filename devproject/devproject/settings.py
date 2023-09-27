8eac25706c84e944f94f5eb247838a17b9a001be
Phadlaan <pqh.suema@gmail.com>
2023-09-28 06:37:20 +0700

    edit html

--- devproject/devproject/settings.py
+++ devproject/devproject/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y%q$o53hdubg)wxcy^@8rpf7+h^v^aag=g4s6kjnyyu*tnrqp6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
-DEBUG = os.environ.get("DEBUG","False").lower()=='true'
 
-ALLOWED_HOSTS = [os.environget("ALLOWED_HOST")].split(" ")
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
